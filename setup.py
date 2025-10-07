3c829d178f7f43613f687c1d00102b14cf2b2696
Matthew Smith <Matthew.Smith@astro.cf.ac.uk>
2025-10-07 14:49:56 +0100

    version0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'astroIm',         # How you named your package folder (MyLib)
   packages = ['astroIm'],   # Chose the same as "name"
-  version = '0.6',      # Start with a small number and increase it with every change you make
+  version = '0.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Astropy Wrapper and specialised functions for FIR/Submm Astro Images',   # Give a short description about your library
   author = 'Matthew Smith',                   # Type in your name
